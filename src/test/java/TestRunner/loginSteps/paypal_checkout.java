ab10365eb3b2918c752576961dc95ca1ca2969cf
sakshipathania <62547853+sakshipathania@users.noreply.github.com>
2021-04-08 16:51:21 +0530

    Update paypal_checkout.java

--- src/test/java/TestRunner/loginSteps/paypal_checkout.java
+++ src/test/java/TestRunner/loginSteps/paypal_checkout.java
@@ -737,7 +737,7 @@ public class paypal_checkout extends SetupClass {
 	   Thread.sleep(2000);
 		
 
-		 driver.findElement(By.xpath("//a[contains(.,'My Account')]")).click();
+		/* driver.findElement(By.xpath("//a[contains(.,'My Account')]")).click();
 		 Thread.sleep(3000);
 		 
 		
@@ -790,6 +790,17 @@ try {
 		js.executeScript("arguments[0].scrollIntoView();",delete_profile_coupon);
 		delete_profile_coupon.click();
 		 Thread.sleep(3000);
+	}*/
+		try {
+			WebElement logout = driver.findElement(By.xpath("//a[contains(text(),'Sign Out')]"));
+			if (logout.isEnabled()) {
+				logout.click();
+				Thread.sleep(8000);
+				driver.navigate().refresh();
+				Thread.sleep(2000);
+			}
+		} catch (NoSuchElementException Ext) {
+		}
 	}
 
 }
